addf23f2e3b69cf2676971c3a38a922aaa2e2008
Giulia Santos <85313679+giuliasl@users.noreply.github.com>
2021-06-17 14:49:07 -0300

    Create string_vogais.c

---
+++ string_vogais.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+main() {
+    // declaração da string
+    char palavra [30];
+    int i= 0, qtde=0;
+    // leitura da string
+    printf ("*** Digite a string ***\n");
+    gets (palavra);
+    //laço para percorrer a string
+    while (palavra [i] != '\0'){
+           if (palavra [i] == 'a' || palavra [i] == 'e' || palavra [i] == 'i' || palavra [i] == 'o' || palavra [i] == 'u') 
+           {   
+                 qtde++;         
+           }
+           i++;
+    }
+    printf ("\n A quantidade de vogais da palavra e = %d\n", qtde);
+    system ("PAUSE");
+}
